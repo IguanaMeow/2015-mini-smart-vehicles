198e4c83865301d79597a39afb1773c8d86817eb
Nicolas Kheirallah <nicolas.kheirallah@gmail.com>
2015-04-13 23:18:22 +0200

    Create startup.sh
    Basic Bash script (Not tested)

---
+++ group-3/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#written by Nicolas Kheirallah
+#Not Tested yet,  will be updated
+/usr/bin/lxterminal -e ./supercomponent --cid=111
+/usr/bin/lxterminal -e ./cockpit --cid=111
+/usr/bin/lxterminal -e ./vehicle --cid=111 --freq=14
+/usr/bin/lxterminal -e ./irus --cid=111
