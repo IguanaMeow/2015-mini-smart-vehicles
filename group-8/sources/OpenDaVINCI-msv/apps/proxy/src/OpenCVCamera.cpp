41461edaef7cbc084e2dd9e3177807ddf49d305d
regaleagle <guseagja@student.gu.se>
2015-04-24 19:41:32 +0200

    ChangeOpenCV cam to run when no display

--- group-8/sources/OpenDaVINCI-msv/apps/proxy/src/OpenCVCamera.cpp
+++ group-8/sources/OpenDaVINCI-msv/apps/proxy/src/OpenCVCamera.cpp
@@ -101,7 +101,7 @@ namespace msv {
         if ( (dest != NULL) && (size > 0) && (m_image != NULL) ) {
             ::memcpy(dest, m_image->imageData, size);
 
-            cvShowImage("WindowShowImage", m_image);
+            //cvShowImage("WindowShowImage", m_image);
             cvWaitKey(10);
 
             retVal = true;
