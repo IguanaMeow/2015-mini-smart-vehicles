bff289833c5c5a53f414048d2ccf529fb0658047
zubayergh <zubayer.ie@gmail.com>
2015-05-04 04:35:37 +0200

    Create HLB_to_LLB.ino
    With out own protocol

---
+++ group-5/Arduino/HLB_to_LLB.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+Servo esc, servo;
+int spd = 1500;
+int str = 90;
+String input;
+
+void setup() { 
+  esc.attach(9);
+  servo.attach(8);
+  Serial.begin(9600); 
+  
+ Serial.println(input.toInt()); 
+}
+
+void loop() {
+   esc.writeMicroseconds(spd); 
+   servo.write(str);
+   Serial.println("Speed= "+String(spd)+", steering= "+String(str));
+   delay(10);
+  
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    input = Serial.readStringUntil(',');
+    Serial.println(input);
+    if(input.length()==11){
+       spd = (input.substring(2, 6)).toInt();
+       str = (input.substring(7, 11)).toInt();
+    }
+  }
+}
