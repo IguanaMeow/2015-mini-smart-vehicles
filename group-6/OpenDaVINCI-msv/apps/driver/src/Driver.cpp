1fc335ced91009103b95cdfbe98ea1f7a68c16c6
erika <erika.cortes.fdl@gmail.com>
2015-05-07 12:47:33 +0200

    Chahges in CMakeList.txt

--- group-6/OpenDaVINCI-msv/apps/driver/src/Driver.cpp
+++ group-6/OpenDaVINCI-msv/apps/driver/src/Driver.cpp
@@ -16,9 +16,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
+// LANE DETECTION //
 #include <stdio.h>
-#include <time.h>
+#include <sys/time.h>
 #include <ctime>
 #include <math.h>
 
@@ -32,7 +32,6 @@
 
 #include "Driver.h"
 
-
 namespace msv {
 
         using namespace std;
@@ -41,57 +40,50 @@ namespace msv {
         using namespace core::data::control;
         using namespace core::data::environment;
 
-/*Calculate distance between obstacles*/
         int counter = -1;
-        int parkingParallel, init;
-        // double gap;
+        int parkingParallel;
+        int start;
+
         double distanceBetweenObjects;
         double distance;
         timeval curTime;
         timeval timer;
 
-/*Sensors data*/
-        double Infrared_FrontRight;
-        double Infrared_RearRight;
-        double Infrared_Rear;
-        double UltraSonic_FrontCenter;
+        double IR_FrontRight;
+        double IR_RearRight;
+        double IR_Rear;
+        double US_FrontCenter;
 
-/*Parking mode*/
-        // int gap; 
-        bool parking;       
-        
+        double angle;
+        double speed;
 
         Driver::Driver(const int32_t &argc, char **argv) :
-            ConferenceClientModule(argc, argv, "Driver") {
+                ConferenceClientModule(argc, argv, "Driver") {
         }
 
         Driver::~Driver() {}
 
         void Driver::setUp() {
             parkingParallel = 0;
-            init = 0;
-            // distance = 0;
-            parking = true;
-            // This method will be call automatically _before_ running body().
+            start = 0;
+                // This method will be call automatically _before_ running body().
         }
 
         void Driver::tearDown() {
-             // gap = 0;
-            // This method will be call automatically _after_ return from body().
+                // This method will be call automatically _after_ return from body().
         }
 
         // This method will do the main data processing job.
         ModuleState::MODULE_EXITCODE Driver::body() {
 
-            while (getModuleState() == ModuleState::RUNNING) {
+                while (getModuleState() == ModuleState::RUNNING) {
                 // In the following, you find example for the various data sources that are available:
 
                 // 1. Get most recent vehicle data:
                 Container containerVehicleData = getKeyValueDataStore().get(Container::VEHICLEDATA);
                 VehicleData vd = containerVehicleData.getData<VehicleData> ();
-                cerr << "Most recent vehicle data: '" << vd.getPosition() << "'" << endl;
+                cerr << "Most recent vehicle data: '" << vd.toString() << "'" << endl;
 
-                
                 // 2. Get most recent sensor board data:
                 Container containerSensorBoardData = getKeyValueDataStore().get(Container::USER_DATA_0);
                 SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
@@ -107,51 +99,47 @@ namespace msv {
                 SteeringData sd = containerSteeringData.getData<SteeringData> ();
                 cerr << "Most recent steering data: '" << sd.toString() << "'" << endl;
 
-                
-                //Sensors aoutput
-                Infrared_FrontRight = sbd.getValueForKey_MapOfDistances(0);
-                cout << "Infrared_FrontRight distance:" << Infrared_FrontRight <<endl;
-
-                Infrared_RearRight = sbd.getValueForKey_MapOfDistances(2);
-                cout << "Infrared_RearRight distance:" << Infrared_RearRight <<endl;
-
-                Infrared_Rear = sbd.getValueForKey_MapOfDistances(1);
-                cout << "Infrared_Rear distance: " << Infrared_Rear << endl;
-
-                UltraSonic_FrontCenter = sbd.getValueForKey_MapOfDistances(3);
-                cout << "UltraSonic_FrontCenter distance: " << UltraSonic_FrontCenter << endl;
-
                 //Setting up Timer //http://linux.die.net/man/2/gettimeofday
                 gettimeofday(&curTime,NULL);
                 distanceBetweenObjects = (curTime.tv_sec - timer.tv_sec) * 1000.0;
                 distanceBetweenObjects += (curTime.tv_usec - timer.tv_usec)/1000.0;
-                // distanceBetweenObjects = gap * vc.setSpeed();
 
                 // Design your control algorithm here depending on the input data from above.
+
+
+
                 // Create vehicle control data.
+
                 VehicleControl vc;
-                // // PARKING = get distance between the spaces 
-                double desiredSteeringWheelAngle = 0;
-                vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
-                vc.setSpeed(0);
-                //Measure the distance between each space.
+
+                 // With setSpeed you can set a desired speed for the vehicle in the range of -2.0 (backwards) .. 0 (stop) .. +2.0 (forwards)
+                vc.setSpeed(sd.getSpeedData());
+                sd.setSpeedData(speed);
+
+                // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
+                double desiredSteeringWheelAngle = sd.getExampleData();
+                sd.setExampleData(angle);
+
+                speed = 4;
+///hena         
                 switch(parkingParallel){
 
                     case 0:
-                        if(init == 0){
+                        if(start == 0){
                             gettimeofday(&timer, NULL);
-                            init = 1;
+                            start = 1;
                         }
 
-                        vc.setSpeed(2.4);
+                        // vc.setSpeed(2.4);
+                        speed = 2.4;
 
-                        if((sbd.getValueForKey_MapOfDistances(2) <= -1) && (distanceBetweenObjects * vc.getSpeed() < 6000)){
+                        if((sbd.getValueForKey_MapOfDistances(2) <= -1) && (distanceBetweenObjects * speed < 6000)){
                             cerr << "Gap: ";
-                            cerr << "Current distance: '" << distanceBetweenObjects * vc.getSpeed() << " cm' " << endl;
-                        }else if(distanceBetweenObjects * vc.getSpeed() >= 6000){
+                            cerr << "Current distance: '" << distanceBetweenObjects * speed << " cm' " << endl;
+                        }else if(distanceBetweenObjects * speed >= 6000){
                             cerr << "Spot found..." << endl;
-                            vc.setSpeed(0);
-                            distance = distanceBetweenObjects * vc.getSpeed();
+                            speed = 0;
+                            distance = distanceBetweenObjects * speed;
 
                             cerr << "Distance: '" << distanceBetweenObjects << " cm'" << endl;
                         }else {
@@ -162,15 +150,15 @@ namespace msv {
                     case 1: 
                         if((distanceBetweenObjects >= 7000) && (distanceBetweenObjects < 14900)){
                             cerr << "Let's park...!";
-                            vc.setSpeed(-1);
+                            speed = -1;
                             vc.setSteeringWheelAngle(19 * Constants::DEG2RAD);    
                         }else if((distanceBetweenObjects >= 14900) && (distanceBetweenObjects < 24000) ){//&& (sbd.getValueForKey_MapOfDistances(1) <= 1.4) 
                             cerr << "Parking...!";
-                            vc.setSpeed(-0.3);
+                            speed = -0.3;
                             vc.setSteeringWheelAngle(-26 * Constants::DEG2RAD);    
                         }else if((distanceBetweenObjects >= 24000) && (distanceBetweenObjects < 30300)){
                             cerr << "Almost...!";
-                            vc.setSpeed(0.3);
+                            speed = 0.3;
                             vc.setSteeringWheelAngle(19 * Constants::DEG2RAD);   
                         }else if(distanceBetweenObjects >= 30300){
                             cerr << "Done!!!"; 
@@ -180,14 +168,19 @@ namespace msv {
                              
                 }
 
-
+             
+               
+             //    double speed =spd.getSpeedData();
 
                 // With setSpeed you can set a desired speed for the vehicle in the range of -2.0 (backwards) .. 0 (stop) .. +2.0 (forwards)
-               // vc.setSpeed(0.5);
+                // vc.setSpeed(sd.getSpeedData());
+                // sd.setSpeedData(speed);
 
                 // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
-                // double desiredSteeringWheelAngle = 5.5; // 4 degree but SteeringWheelAngle expects the angle in radians!
-                // vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                // double desiredSteeringWheelAngle = sd.getExampleData();
+                // sd.setExampleData(angle);
+               // double desiredSteeringWheelAngle = 0; // 4 degree but SteeringWheelAngle expects the angle in radians!
+                vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
 
                 // You can also turn on or off various lights:
                 vc.setBrakeLights(false);
@@ -198,19 +191,8 @@ namespace msv {
                 Container c(Container::VEHICLECONTROL, vc);
                 // Send container.
                 getConference().send(c);
-            }
+                }
 
-            return ModuleState::OKAY;
+                return ModuleState::OKAY;
         }
 } // msv
-
-                /* ID Sensors
-                0 = Infrared_FrontRight
-                1 = Infrared_Rear
-                2 = Infrared_RearRight
-
-                3 = UltraSonic_FrontCenter
-                4 = UltraSonic_FrontRight
-                5 = UltraSonic_RearRight
-                */
-
