310dab15d258db757a9c7d3c44ebe03d81907a31
guslidstmi <guslidstmi@student.gu.se>
2015-05-12 10:28:53 +0200

    fixed issues with leftList not updating x values

--- group-8/sources/OpenDaVINCI-msv/apps/lanedetector/src/LaneDetector.cpp
+++ group-8/sources/OpenDaVINCI-msv/apps/lanedetector/src/LaneDetector.cpp
@@ -247,6 +247,12 @@ void LaneDetector::processImage() {
 		leftLine3.setCritical(leftList[2].getCritical());
 		leftLine4.setCritical(leftList[3].getCritical());
 
+		leftList[0].setXPos(leftLine1.setXPos());
+		leftList[1].setXPos(leftLine2.setXPos());
+		leftList[2].setXPos(leftLine3.setXPos());
+		leftList[3].setXPos(leftLine4.setXPos());
+
+
 		critAngleRight = (atan2(rightLine2.getYPos() - rightLine1.getYPos(), rightLine1.getXPos() - rightLine2.getXPos()) * Constants::RAD2DEG);
 	}
 	cout << "crit distance: " << rightLine1.getCritical() << "critAngle: " << critAngleRight << endl;
