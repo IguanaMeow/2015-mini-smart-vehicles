4aa0f8b39dfe4ca95c951e76bcdb4f02cd2566ed
jas0n- <jasonkuan87@gmail.com>
2015-05-13 12:50:04 +0200

    Cleaned-up Herbie-Repository

--- group-7/opendavinci-supercomponent-config/runOpenDavinci.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/usr/bin/lxterminal -e ./supercomponent --cid=111 --freq=20
-/usr/bin/lxterminal -e ./vehicle --cid=111 --freq=20
-/usr/bin/lxterminal -e ./cockpit --cid=111 --freq=20
-/usr/bin/lxterminal -e ./irus --cid=111 --freq=20
