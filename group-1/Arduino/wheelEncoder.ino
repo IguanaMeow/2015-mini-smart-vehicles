05172c68c01a9d6c794e78b7c5f29b849b5d6eec
nedwed123 <nedwed123@users.noreply.github.com>
2015-05-13 16:25:38 +0200

    Update wheelEncoder.ino
    Changed attachInterrupt function's parameter from "CHANGE" to "RISING" due to noisy data.

--- group-1/Arduino/wheelEncoder.ino
+++ group-1/Arduino/wheelEncoder.ino
@@ -1,11 +1,17 @@
+/*
+This code reads tick signals from the wheel encoder Plolulu 
+using interrupts via Outport A only (NOt using poth outPOrts 
+of the wheel encoder )
 
+Author: Neda Ashrafi Amiri
+*/
 volatile unsigned long ticks = 0; 
 
 void setup()
 {
   Serial.begin(9600);
    
-  attachInterrupt(5, encoderInterrupt, CHANGE); // int 5 represents pin 18 on mega
+  attachInterrupt(5, encoderInterrupt, RISING); // int 5 represents pin 18 on mega
 }
 
 void loop()
