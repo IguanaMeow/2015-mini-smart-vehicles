b64ce722c5c652919457eb06b40f1af5c3291f36
KittenAvenger <nevadaa51@yahoo.se>
2015-05-18 22:01:25 +0200

    Update sensors.ino

--- group-11/hardware/sensors.ino
+++ group-11/hardware/sensors.ino
@@ -1,101 +1,102 @@
-#include <Wire.h>
-#define FRONT_SONAR 0x72
-#define FRONT_RIGHT_SONAR 0x70
+unsigned long before;
+unsigned long after;
+unsigned long diff;
 
-#define REAR_IR 2
-#define FRONT_RIGHT_IR 1
-#define REAR_RIGHT_IR 0
+#define REAR_IR 0
+#define FRONT_RIGHT_IR 2
+#define REAR_RIGHT_IR 1
 #define VoltsToCentimeters(volts) ((2914 / (volts + 5)) - 1)
-
-#define cmdByte 0x00                              // Command byte
-#define lightByte 0x01                            // Byte to read light sensor
-#define rangeByte 0x02
 
 int IR_front_right, IR_rear_right, IR_rear;
 
+// this constant won't change.  It's the pin number
+// of the sensor's output:
+const int USFtrig = 5;
+const int USFecho = 6;
+const int USFRtrig = 4;
+const int USFRecho = 7;
+
+
+int sonarFront;
+int sonarFrontRight;
+
 void setup() {
-  Wire.begin();
+  
   Serial.begin(9600);
+  pinMode(USFecho, INPUT); 
+  pinMode(USFtrig, OUTPUT);
+  pinMode(USFRecho, INPUT);
+  pinMode(USFRtrig, OUTPUT); 
   delay(100);
 }
 
-void loop() {
-  
-  IR_rear_right = IR_Distance(REAR_RIGHT_IR);
-  IR_rear = IR_Distance(REAR_IR);
-  IR_front_right= IR_Distance(FRONT_RIGHT_IR);
-  
-  Serial.print("IR rear right distance: ");
-  Serial.print(IR_rear_right);
-  Serial.print(" cm");
-  Serial.print("\t");
-  
-  Serial.print("IR front right distance: ");
-  Serial.print(IR_front_right);
-  Serial.print(" cm");
-  Serial.print("\t");
-  
-  Serial.print("IR rear distance: ");
-  Serial.print(IR_rear);
-  Serial.print(" cm");
-  Serial.print("\t");
-  
-  Serial.println();
-  //delay(50);
-  startSonars();
-  int sonarFront = getRange(FRONT_SONAR);                      // Calls a function to get the range data 
-  Serial.print("Front sonar distance: ");
-  Serial.print(sonarFront);
-  Serial.print(" cm");
-  Serial.print("\t");
-  
-  int sonarFrontRight = getRange(FRONT_RIGHT_SONAR);                      // Calls a function to get the range data 
-  Serial.print("Front right sonar distance: ");
-  Serial.print(sonarFrontRight);
-  Serial.print(" cm");
-  
-  Serial.println();
-  
-  delay(700);
+void loop()
+{
+   before= millis(); 
+   String format = "";
+   
+     // IRT, IRB & IRR
+    IR_front_right = IR_Distance(FRONT_RIGHT_IR);
+    IR_rear_right = IR_Distance(REAR_RIGHT_IR);
+    IR_rear = IR_Distance(REAR_IR);  
 
+    sonarFront = US_getRange(USFtrig, USFecho);
+    sonarFrontRight = US_getRange(USFRtrig, USFRecho); 
+   
+  
+  
+  // Creating temporary concatenation variables for each sensor
+     String FUS = String(sonarFront);
+     String FRUS = String(sonarFrontRight); 
+     String IRT = String(IR_front_right);
+     String IRB = String(IR_rear_right);
+     String IRR = String(IR_rear);
+
+      
+     format = FUS + " " + FRUS + " " + IRT + " " + IRB + " " + IRR + " ";
+     //  format = IRT + " " + IRB + " " + IRR + " ";
+    
+     Serial.println(format);  
+    
+    after=millis();
+    diff = after - before;
+    int time;
+    
+    if(diff < 50 )
+    {
+      time = 50 - diff;
+      delay(time);
+    }
 }
 
-int IR_Distance(int IR_sensor){
+
+// Auxiliary function to acquire IR distance: 
+int IR_Distance(int IR_sensor)
+{
   float sensorValue = 0;
   sensorValue = analogRead(IR_sensor);
   int distance = VoltsToCentimeters(sensorValue);
   
-  if(distance < 40){
-  return(distance);
+  if(distance < 40)
+  {
+    return(distance);
   }
+  
   return(-1);
 }
 
-void startSonars()
+// Auxiliary function to get US range:
+int US_getRange(int trigPin, int echoPin) 
 {
-  Wire.beginTransmission(0x00);             // Start communicating with SRF08
-  Wire.write((byte)cmdByte);                             // Send Command Byte
-  Wire.write(0x51);                                // Send 0x51 to start a ranging in cm
-  Wire.endTransmission();
- 
-  delay(80);
+  digitalWrite(trigPin, HIGH); //Trigger ultrasonic detection 
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW); 
+  int distance = pulseIn(echoPin, HIGH, 10000); //Read ultrasonic reflection
+  distance= distance/58; //Calculate distance 
+  
+  if(distance <= 0 ) return -1;
+  return distance;  
 }
 
-int getRange(unsigned char srfAddress){                                   // This function gets a ranging from the SRF08
-  int range = 0; 
- 
-                                     // Wait for ranging to be complete
- 
-  Wire.beginTransmission(srfAddress);             // start communicating with SRFmodule
-  Wire.write(rangeByte);                           // Call the register for start of ranging data
-  Wire.endTransmission();
- 
-  Wire.requestFrom(srfAddress, 2);                // Request 2 bytes from SRF module
-  while(Wire.available() < 2);                    // Wait for data to arrive
-  byte highByte = Wire.read();                 // Get high byte
-  byte lowByte = Wire.read();                  // Get low byte
- 
-  range = (highByte << 8) + lowByte;              // Put them together
- 
-  return(range);                                  // Returns Range
-}
+
+  
