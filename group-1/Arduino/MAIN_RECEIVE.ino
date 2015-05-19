a3b2434d970020782709821cb376bb7f816a99c7
nedwed123 <nedwed123@users.noreply.github.com>
2015-05-19 11:45:42 +0200

    Update MAIN_RECEIVE.ino
    edited the decodedNetstring

--- group-1/Arduino/MAIN_RECEIVE.ino
+++ group-1/Arduino/MAIN_RECEIVE.ino
@@ -32,7 +32,7 @@ void loop() {
 void serialEvent() {
   if(Serial.available()) {
     c = Serial.readStringUntil(',');
-    decodedS=decodedNetstring(c);
+    decodedS=decodedString(c);
     delay(2);  //slow looping to allow buffer to fill with next character
 
 
@@ -48,16 +48,13 @@ void serialEvent() {
 
 
 
-String decodedNetstring(String netstring){
-  if (netstring.length() < 3) return "error"; //if the Netstring is less than 3 characters, it's either an invalid one or contains an empty string
-  int semicolonIndex = netstring.indexOf(':');
-  if (semicolonIndex < 0) return "error"; // if there's no semicolon, then it's an invalid Netstring
-  String parsedDigits = netstring.substring(0, semicolonIndex); //parse until the semicolon. Those should be the control digits
-  int controlDigits = parsedDigits.toInt();
-  if (controlDigits < 1) return "error"; //if the control digit is smaller than 1, then it's either not a digit or the Netstring is invalid
-  String command = netstring.substring(semicolonIndex+1); //parse after the semicolon until the end of the string
+String decodedString(String string){
+  if (string.length() < 8) return "error"; //if string is less than 8 chars, it's either invalid or empty string
+                                              //e.g. 512030, ---> this is alltogether 8 characters containing /0
+  int controlDigits = 6; // we always have a checksum of 6 by default
+  String command = string; 
   if (!command.length()) return "error"; // if it's an empty string, return "error"
-  //if (command.substring(command.length() -1) == ",") command.remove(command.length()-1); //if last character is a comma, remove it
+  if (command.substring(command.length() -1) == ",") command.remove(command.length()-1); //if last character is a comma, remove it
   if (command.length() != controlDigits) return "error"; //if string's length isn't equal with the control digits, it's an invalid Netstring
   return command;
 }
