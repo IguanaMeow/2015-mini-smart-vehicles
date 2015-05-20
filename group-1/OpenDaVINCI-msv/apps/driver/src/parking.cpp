0795cf62f72984217f402eab820300a3511a26df
IguanaMeow <iguanabg92@gmail.com>
2015-05-20 13:28:24 +0200

    Updated for acceptance tests

--- group-1/OpenDaVINCI-msv/apps/driver/src/parking.cpp
+++ group-1/OpenDaVINCI-msv/apps/driver/src/parking.cpp
@@ -39,54 +39,41 @@ namespace msv {
         using namespace core::data::control;
         using namespace core::data::environment;
 
-        double pi = 3.14159265;
-        float start;
-        float end;
-        float eee;
-        float mmm;
-        int trig = 0;
-        //float start2;
-        //float es;
-        float xd;  // distance the car needs to drive from point a before turning stearing wheel
-        //float xdc;
-      
-        // sensors for y with travelpath
-        float esy;
-        float esz;
-        float ystart;
-        float yend;
-        double Rs1; //first steer angle radius of curvature
-        double Rs2; //second steer angle radius of curvature
-
-
-        float qstart;
-        float qend;
-        float qesy;
-
-        int state=0;
-        int parking1=0;
-        int parking2 =0;
+        double pi = 3.14159265; // pi
+        float start;    // start, starts calculating the traveled path 
+        float end;      // end, stops calculating the traveled path 
+        float eee;      // result of end - start
+        float mmm;      // yend+2.4; //controll distance of first curvature
+        float xd;       // distance the car needs to drive from point the beginning of state 3 before turning stearing wheel
+        float esy;      // result of yend - ystart
+        float ystart;   // ystart, starts calculating the traveled path a second time
+        float yend;     // yend, stops calculating the traveled path a second time
+        double Rs1;     // first steer angle radius of curvature
+        double Rs2;     // second steer angle radius of curvature
+        float qend;     // AbsTraveledPath - mmm;
+        int state=0;    // states to transitions through if loops
+        int parking1=0; // controles parking state on or off
 
         
 
         Driver::Driver(const int32_t &argc, char **argv) :
-	        ConferenceClientModule(argc, argv, "Driver") {
+            ConferenceClientModule(argc, argv, "Driver") {
         }
 
         Driver::~Driver() {}
 
         void Driver::setUp() {
-	        // This method will be call automatically _before_ running body().
+            // This method will be call automatically _before_ running body().
         }
 
         void Driver::tearDown() {
-	        // This method will be call automatically _after_ return from body().
+            // This method will be call automatically _after_ return from body().
         }
 
         // This method will do the main data processing job.
         ModuleState::MODULE_EXITCODE Driver::body() {
 
-	        while (getModuleState() == ModuleState::RUNNING) {
+            while (getModuleState() == ModuleState::RUNNING) {
                 // In the following, you find example for the various data sources that are available:
 
                 // 1. Get most recent vehicle data:
@@ -116,10 +103,10 @@ namespace msv {
 
 
                 // Create vehicle control data.
-                VehicleControl vc;	//vc is new car
+                VehicleControl vc;  //vc is new car
 
                 // With setSpeed you can set a desired speed for the vehicle in the range of -2.0 (backwards) .. 0 (stop) .. +2.0 (forwards)
-              //  vc.setSpeed(1.0);	//car with speed of 1.0
+              //  vc.setSpeed(1.0); //car with speed of 1.0
                 
                 // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
                 double desiredSteeringWheelAngle = sd.getExampleData(); // 4 degree but SteeringWheelAngle expects the angle in radians!
@@ -127,21 +114,15 @@ namespace msv {
 
 
 
-                double IR_FR = sbd.getValueForKey_MapOfDistances(0);	//ir front right
+                double IR_FR = sbd.getValueForKey_MapOfDistances(0);    //ir front right
                 double IR_R = sbd.getValueForKey_MapOfDistances(1);   //ir rear rear
-                double IR_RR = sbd.getValueForKey_MapOfDistances(2);	//ir rear right
+                double IR_RR = sbd.getValueForKey_MapOfDistances(2);    //ir rear right
                 double US_FC = sbd.getValueForKey_MapOfDistances(3);  //ultra sonic front cener
-                double US_FR = sbd.getValueForKey_MapOfDistances(4);	//ultra sonic front right
-                double US_RR = sbd.getValueForKey_MapOfDistances(5);	//ultra sonic rear right
-                //std::cout << "Sensor infrared front right"<< x << std::endl;
-
-
-                //double cotan(double i) { return(1 / tan(i)); }
+                double US_FR = sbd.getValueForKey_MapOfDistances(4);    //ultra sonic front right
+                double US_RR = sbd.getValueForKey_MapOfDistances(5);    //ultra sonic rear right
                 
                 double cl;  //car length
                 cl = 4;
-            //    double cw;  //car width
-            //    cw = 2;
                 double wl;  //length between front and back car wheel axels
                 wl = 2.65;
                 double ww;  //width between front or back car wheels
@@ -150,120 +131,77 @@ namespace msv {
                 wfbf = 1;
                 double fcr;  //full circle radious when car wheels at max angle
                 fcr = 5.25;
-                
-            //    double ol;  //object length
-            //    ol = 7;
                 double ow;  //object width
                 ow = 3.5;
-            //    double owl; //object wheel length
-            //    owl = 4.5;
-            //    double oww; //object wheel width
-            //    oww = 2.5;
                 
                 double parkspace;   //calculates the space needed to park
                 parkspace = cl + sqrt((pow (fcr, 2) - pow(wl, 2))+(2+ pow(ww, 2))-pow((sqrt(pow(fcr, 2)- pow(wl, 2)-ow)), 2)-wl-wfbf);
                 
                 Rs1 = (wl*(tan(25*pi/180.0)))+ww/2;     //first steer angle radius of curvature
                 Rs2 = (wl*(tan(25*pi/180.0)))+ww/2;     //second steer angle radius of curvature
-                
-                // cot(1/tan(45*pi/180.0)) - cot(1/(tan(45*pi/180.0)) = ww/wl
-              //  double xc;
-            //  xc = IR_FR; // distance between cars from ir
 
                 eee = end - start;
 
-                double aq1;    // distance aq
-                double aq2;
-                float aaqq;
-              //  aq = cw +xc;       
+                double aq1;    // radius of the curvature corresponding to the first steering angle
+                double aq2;    // radius of the curvature corresponding to the second steering angle
+                float aaqq;    // 
                 aq1 = Rs1 - ww/2;
                 aq2 = Rs2 + ww/2;
                 aaqq = (aq2-aq1)+2.3;
-                xd = (pow((Rs1*Rs2),2)-pow((Rs1),2))+6;
+                xd = (pow((Rs1*Rs2),2)-pow((Rs1),2))+5.6;   // 
 
-                //double aa;  // angle of stearing wheel at trajectory b
-                //aa = atan(xd/Rs1)* 180/ pi;
-               //double bb;
-               // bb = aa;
-               //double lpo;
-               //double loq;
-               // lpo = (aa / 360) * 2*pi * (Rs1 - ww/2);
-               // loq = (bb / 360) * 2*pi * (Rs2 - ww/2);
-                // double FF;   // missing allignment mismagment of car
-                 // FF = atan((xd/Rs1)/d)*pi/180;
-
-                /*if(US_FR<8){
-                    parking1=1;
-                    parking2=0;
-                }
-                else 
-                    parking2=1;
-                    parking1=0;
-                }*/
 
   
-                if(parking1 ==0 && IR_FR>0){	
+                if(parking1 ==0 && IR_FR>0){    
                     vc.setSpeed(1.0);
-                        start = vd.getAbsTraveledPath();	//start calculating the traveled path
+                        start = vd.getAbsTraveledPath();    //start calculating the traveled path
                 }
-                if(IR_FR<0 && trig == 0){
+                if(IR_FR<0){
                     vc.setSpeed(1.0);
-                        end = vd.getAbsTraveledPath();		//stop calculating the traveled path
+                        end = vd.getAbsTraveledPath();      //stop calculating the traveled path
                 }
-                 if(IR_RR>0 && state==0 && IR_FR<0){      //state is 0 when ir_rr  and ir_fr is -1
+                 if(state==0 && IR_RR>0 && IR_FR<0){      //state is 0 when ir_rr  and ir_fr is -1
                   state=1; 
                     vc.setSpeed(1.0);
                 }
-                if(IR_RR<0 && state==1){                //state is 1 when is_rr is bigger than 0
+                if(state==1 && IR_RR<0){                //state is 1 when is_rr is bigger than 0
                     std::cout << "STATE 1"<< state << std::endl;
                         ystart = vd.getAbsTraveledPath();    //start calculating the traveled path
                         state=2;
                         vc.setSpeed(1.0);
                 }
-                if(IR_RR>0 && state==2 && trig ==0){
+                if(state==2 && IR_RR>0){
                     std::cout << "STATE 2"<< state << std::endl;
                         yend = vd.getAbsTraveledPath();      //stop calculating the traveled path
+                        mmm = yend+2.4; //controll distance
                         vc.setSpeed(1.0);
                 }
                 esy = yend - ystart;
-                 /******** change back xd(delete 30)  */ 
-               if(esy > (xd+30) && xd > parkspace && trig ==0 ){       // if travel path esy is bigger than xd path and if xd path
+               if(esy > (xd+3) && xd > parkspace){       // if travel path esy is bigger than xd path and if xd path
                     vc.setSpeed(0.0);
-                    
                     state =3;
                 }
-                else if(xd>esy && esy > parkspace && trig ==0 && parking2==0){
-                    vc.setSpeed(0.0);
-                    trig=55; // not needed ???
-                    parking2=1;
-                    parking1=2;
-                    state=8;
+                if(state ==3 && US_RR>-1){                                     //in state 3,
+                    vc.setSpeed(1.0);
                 }
-                if(state ==3  && trig ==0){                                     //in state 3,
+                if(state ==3 && US_RR<0){                                     //in state 3,
                     std::cout << "STATE 3"<< state << std::endl;
-                    mmm = 25.7;  // fix> how to controll distance //distance mmm is 25.2,
                     vc.setSteeringWheelAngle(26 * 10);              //the car wheels turn max to the right,
                     vc.setSpeed(-0.5);
                     qend =vd.getAbsTraveledPath()-mmm;
                     state =4;
                 }
-                if(state ==4  &&  qend  >= aaqq && trig ==0){
+                if(state ==4  &&  qend  >= aaqq){
                     std::cout << "STATE 4"<< state << std::endl;
                     vc.setSpeed(0.0); 
                     vc.setSteeringWheelAngle(-26 * 10);
                     vc.setSpeed(-0.5);
                     state =5;
                 }
-                if(state == 5 && (IR_R > 0.6 && IR_R <=1.9) && trig == 0){
-                    trig =1; // increase trigger to not go back to state less then 5
+                if(state == 5 && (IR_R > 0.6 && IR_R <=1.9)){
                     std::cout << "STATE 5"<< state << std::endl; 
                     vc. setSpeed(0.0);
                     state =6;
-                }
-                if(state == 6 && US_FC > 2 && trig == 1){
-                    std::cout << "STATE 6"<< state << std::endl;
-                    vc.setSteeringWheelAngle(27 * 10);
-                    vc.setSpeed(0.5);
                 }
                 else if (state==6 &&  US_FC < 2.1) {
                     std::cout << "STATE 6"<< state << std::endl;
@@ -275,89 +213,26 @@ namespace msv {
                     vc.setSpeed(-0.5);                       
                 }
                 else if(state == 7 && IR_R >2.3){
-                    std::cout << "PARKING DONE "<< state << std::endl;
+                    std::cout << "PARKING DONE - STATE "<< state << std::endl;
                     vc.setSpeed(0.0);
                 }
-                
-
-//Y-START 10.1442
-//Y-END 22.2443
-//Parking Distance ESY 12.1001
-//XD XD XD XD 12.0637
-//Parking Space 5.68819
-
-//Y-START 16.3954
-//Y-END 21.5377
-//Parking Distance ESY 5.14239
-//XD XD XD XD 12.0637
-//Parking Space 5.68819
-
-
-                // parking2, if parallel parking is not enough space then park backwards...
-            
-               else if(parking2 == 1 && state ==8){
-                    std::cout << "PARKING state 2222 "<< state << std::endl;
-                    vc.setSpeed(0.0);
-                    parking2 =3;
-                    state =10;
-                
-                     
-
-    }   
-
-    // check if other sensor can be used instead ...
-    // STATE 10: Go forwared left in the if statement below
-    if (state ==10 && parking2==3 && US_RR <10){
-        vc.setSpeed(0.5);
-        vc.setSteeringWheelAngle(-0.2);
         
-    // STATE 10: Go backwards Right in the if statement below
-    } if (state ==10 && parking2==3 && IR_RR >2.35) {
-        vc.setSpeed(-0.5);
-        vc.setSteeringWheelAngle(0.23);
-        state=11;
-    //
-    }if (state ==11 && parking2==3 && (IR_RR > 1.5 && IR_RR < 2.6)) {
-        vc.setSpeed(-0.5);
-        vc.setSteeringWheelAngle(0.3);
-       } else if( state==11 && parking2==3 && IR_RR <1.5){
-        vc.setSpeed(-0.5);
-        vc.setSteeringWheelAngle(0.5);
 
-       }if(state==11 && parking2==3 && IR_FR <1 && IR_RR <1.23 && US_FR <1){
-         vc.setSpeed(0.0);
-        state=12;
-
-       }
-        
-            
-            
-          
-
-                    
-
-                //std::cout << "START "<< start << std::endl;
-                //std::cout << "END "<< end << std::endl;
+                std::cout << "START "<< start << std::endl;
+                std::cout << "END "<< end << std::endl;
                 std::cout << "Y-START "<< ystart << std::endl;
                 std::cout << "Y-END "<< yend << std::endl;
                 std::cout << "Parking Distance ESY "<< esy << std::endl;
                 //std::cout << "----------"<< xd<< std::endl;
-                //std::cout << "Q-START "<< qstart << std::endl;
-                //std::cout << "QESY "<< qesy << std::endl;
-                //std::cout << "MMM "<< mmm << std::endl;
-                //std::cout << "AAQQ "<< aaqq << std::endl;
+                std::cout << "Q-end "<< qend << std::endl;
+                std::cout << "MMM "<< mmm << std::endl;
+                std::cout << "AAQQ "<< aaqq << std::endl;
                 //std::cout << "----------"<< xd<< std::endl;
                 
                 std::cout << "XD XD XD XD "<< xd << std::endl;
-
-                 
-                //std::cout << "Rs1 <<<<<<<<<<<<< "<< Rs1 << std::endl;
-                //std::cout << "aq1 <<<<<<<<<<<<< "<< aq1 << std::endl;              
-                //std::cout << "aq2 <<<<<<<<<<<<< "<< aq2 << std::endl;
                 std::cout << "Parking Space "<< parkspace << std::endl;
                 
                 std::cout << "PARKING111111 "<< parking1 << std::endl;
-               // std::cout << "PARKING222222 "<< parking2 << std::endl;
                 std::cout << "STATE "<< state << std::endl;
                 std::cout << "IR_FR "<< IR_FR << std::endl;
                 std::cout << "IR_RR "<< IR_RR << std::endl;
@@ -365,10 +240,6 @@ namespace msv {
                 std::cout << "US_FC "<< US_FC << std::endl;
                 std::cout << "US_FR "<< US_FR << std::endl;
                 std::cout << "US_RR "<< US_RR << std::endl;
-
-                // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
-                //double desiredSteeringWheelAngle = 4; // 4 degree but SteeringWheelAngle expects the angle in radians!
-                //vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
 
                 // You can also turn on or off various lights:
                 vc.setBrakeLights(false);
@@ -379,9 +250,8 @@ namespace msv {
                 Container c(Container::VEHICLECONTROL, vc);
                 // Send container.
                 getConference().send(c);
-	        }
+            }
 
-	        return ModuleState::OKAY;
+            return ModuleState::OKAY;
         }
 } // msv
-
