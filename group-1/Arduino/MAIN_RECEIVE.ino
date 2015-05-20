800fa0040403c6a86f403e06359645b8d1f2d628
nedwed123 <nedwed123@users.noreply.github.com>
2015-05-20 15:45:52 +0200

    changed baud rate and fixed decoding

--- group-1/Arduino/MAIN_RECEIVE.ino
+++ group-1/Arduino/MAIN_RECEIVE.ino
@@ -3,7 +3,6 @@ This code receives an encoded Netstring message and
 decodes it acording to Netsring protocols. Then the 
 decoded data is transferred to two servos, one to steer 
 the car and another to move the car at a given speed. 
-
 Author: Neda Ashrafi Amiri
 */
 #include <Servo.h>
@@ -18,7 +17,7 @@ int control =512;
 void setup() {
   esc.attach(9);//attach esc to pin 9 on Mega
   servo.attach(10);// attach servo to pin 10 Mega
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -37,7 +36,7 @@ void serialEvent() {
 
 
     if (decodedS.length() >0) {
-      Serial.println(decodedS);  //so you can see the captured string 
+     // Serial.println(decodedS);  //so you can see the captured string 
       control = decodedS.substring(0, 3).toInt();  //convert a substring readString into a number
       steer= decodedS.substring(3, 6).toInt(); //convert a substring readString into a number
     }
@@ -51,7 +50,7 @@ void serialEvent() {
 String decodedString(String string){
   //if string is less than 8 chars, it's either invalid or empty string
   //e.g. 512030, ---> this is alltogether 8 characters containing /0
-  if (string.length() < 8) return "error"; 
+  if (string.length() < 6) return "error"; 
   // we always have a checksum of 6 by default
   int controlDigits = 6;
   String command = string; 
