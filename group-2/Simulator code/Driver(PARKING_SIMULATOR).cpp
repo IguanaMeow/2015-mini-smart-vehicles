868c06cc6e4bca39508b87397767496f2b2a8868
JHermansson <johan.hermansson@hotmail.com>
2015-05-21 22:29:06 +0200

    Simulator
    Code from the simulator

---
+++ group-2/Simulator code/Driver(PARKING_SIMULATOR).cpp
@@ -0,0 +1,211 @@
+/**
+ * driver - Sample application for calculating steering and acceleration commands.
+ * Copyright (C) 2012 - 2015 Christian Berger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+#include "core/io/ContainerConference.h"
+#include "core/data/Container.h"
+#include "core/data/Constants.h"
+#include "core/data/control/VehicleControl.h"
+#include "core/data/environment/VehicleData.h"
+
+#include "GeneratedHeaders_Data.h"
+
+#include "Driver.h"
+
+double freeSpaceStart = 0;
+double freepath;
+double back0 = 0;
+double back1 = 0;
+int mode = 1;
+double forward = 0;
+
+namespace msv {
+    
+    using namespace std;
+    using namespace core::base;
+    using namespace core::data;
+    using namespace core::data::control;
+    using namespace core::data::environment;
+    
+    Driver::Driver(const int32_t &argc, char **argv) :
+    ConferenceClientModule(argc, argv, "Driver") {
+    }
+    
+    Driver::~Driver() {}
+    
+    void Driver::setUp() {
+        // This method will be call automatically _before_ running body().
+    }
+    
+    void Driver::tearDown() {
+        // This method will be call automatically _after_ return from body().
+    }
+    
+    // This method will do the main data processing job.
+    ModuleState::MODULE_EXITCODE Driver::body() {
+        
+        while (getModuleState() == ModuleState::RUNNING) {
+            // In the following, you find example for the various data sources that are available:
+            
+            // 1. Get most recent vehicle data:
+            Container containerVehicleData = getKeyValueDataStore().get(Container::VEHICLEDATA);
+            VehicleData vd = containerVehicleData.getData<VehicleData> ();
+            cerr << "Most recent vehicle data: '" << vd.toString() << "'" << endl;
+            
+            // 2. Get most recent sensor board data:
+            Container containerSensorBoardData = getKeyValueDataStore().get(Container::USER_DATA_0);
+            SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
+            cerr << "Most recent sensor board data: '" << sbd.toString() << "'" << endl;
+            
+            // 3. Get most recent user button data:
+            Container containerUserButtonData = getKeyValueDataStore().get(Container::USER_BUTTON);
+            UserButtonData ubd = containerUserButtonData.getData<UserButtonData> ();
+            cerr << "Most recent user button data: '" << ubd.toString() << "'" << endl;
+            
+            // 4. Get most recent steering data as fill from lanedetector for example:
+            Container containerSteeringData = getKeyValueDataStore().get(Container::USER_DATA_1);
+            SteeringData sd = containerSteeringData.getData<SteeringData> ();
+            cerr << "Most recent steering data: '" << sd.toString() << "'" << endl;
+            
+            
+            
+            
+            // Create vehicle control data.
+            VehicleControl vc;
+            
+
+            
+            // MY Code
+            ///////////////////////////////////////////////////////////////////////////
+            
+            
+            
+            double abt = vd.getAbsTraveledPath();
+            double sen6 = sbd.getValueForKey_MapOfDistances(0);
+            double sen = sbd.getValueForKey_MapOfDistances(1);
+            double sen2 = sbd.getValueForKey_MapOfDistances(2);
+            double sen3 = sbd.getValueForKey_MapOfDistances(3);
+            double sen4 = sbd.getValueForKey_MapOfDistances(4);
+            double sen5 = sbd.getValueForKey_MapOfDistances(5);
+            double desiredSteeringWheelAngle = 0;
+            cerr <<sen<< "Rear IR"<<endl;
+            cerr <<sen2<< "Rear right IR"<<endl;
+            cerr <<sen6<< "Front right IR"<<endl;
+            cerr <<sen3<< "Front sonic"<<endl;
+            cerr <<sen4<< "Front right sonic"<<endl;
+            cerr <<sen5<< "Rear right sonic"<<endl;
+            cerr <<abt<< "Traveled path"<<endl;
+            cerr <<freeSpaceStart<< "FreeStart path"<<endl;
+            
+            switch(mode){
+                case 1:
+                    vc.setSpeed(0.5);
+                    desiredSteeringWheelAngle = 0;
+                    vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                    if (sen2 > 0){
+                        freeSpaceStart = 0;
+                    }
+                    else if(sen2 < 0){
+                        if(freeSpaceStart <= 0){
+                            freeSpaceStart = abt;
+                            cerr <<freeSpaceStart<< "FreeStart path set"<<endl;
+                        }
+                        freepath = abt - freeSpaceStart;
+                        cerr <<freepath<< "Free = "<<endl;
+                        if(freepath >= 6){
+                            vc.setSpeed(0.0);
+                            mode = 5;
+                        }
+                    }
+                    else{
+                    }
+                    break;
+                case 2:
+                    if (back0 <= 0){
+                        back0 = abt;
+                    }
+                    if ((abt - back0) >= 4.3){
+                        back0 = 0;
+                        mode = 7;
+                    }
+                    desiredSteeringWheelAngle = 25;
+                    vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                    vc.setSpeed(-0.3);
+                    break;
+                case 3:
+                    if (back1 <= 0){
+                        back1 = abt;
+                    }
+                    if (sen > 0 && sen < 1.9){
+                        forward = 0;
+                        mode = 6;
+                    }
+                    desiredSteeringWheelAngle = -25;
+                    vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                    vc.setSpeed(-0.2);
+                    break;
+                case 4:
+                    vc.setSpeed(0.0);
+                    break;
+                case 5:
+                    if(forward <= 0) forward = abt;
+                    vc.setSpeed(0.3);
+                    if ((abt - forward) >= 1.25) mode = 2;
+                    break;
+                case 6:
+                    if(forward <= 0) forward = abt;
+                    if((abt - forward) > 1) mode = 4;
+                    desiredSteeringWheelAngle = 20;
+                    vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                    vc.setSpeed(0.3);
+                    break;
+                case 7:
+                    if (back0 <= 0){
+                        back0 = abt;
+                    }
+                    if ((abt - back0) >= 1.3){
+                        back0 = 0;
+                        mode = 3;
+                    }
+                    desiredSteeringWheelAngle = 0;
+                    vc.setSteeringWheelAngle(desiredSteeringWheelAngle * Constants::DEG2RAD);
+                    vc.setSpeed(-0.3);
+                    break;
+                    
+            }
+            
+          
+            
+            // You can also turn on or off various lights:
+            vc.setBrakeLights(false);
+            vc.setLeftFlashingLights(false);
+            vc.setRightFlashingLights(true);
+            
+            // Create container for finally sending the data.
+            Container c(Container::VEHICLECONTROL, vc);
+            // Send container.
+            getConference().send(c);
+        }
+        
+        return ModuleState::OKAY;
+    }
+} // msv
+
