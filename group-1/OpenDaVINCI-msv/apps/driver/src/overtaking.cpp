162d0f9476c3024763c76756c9b988a3f68d88cb
MahsaAbbasian <mahsa.abbasian@yahoo.com>
2015-05-22 12:08:51 +0200

    Update overtaking.cpp

--- group-1/OpenDaVINCI-msv/apps/driver/src/overtaking.cpp
+++ group-1/OpenDaVINCI-msv/apps/driver/src/overtaking.cpp
@@ -37,14 +37,18 @@ namespace msv {
         using namespace core::data;
         using namespace core::data::control;
         using namespace core::data::environment;
-
+                //checking obstacle in front of the car
                 bool obstacleFront=false;
+                //checking turn to left 
                 bool startTurningToLeft=false;
-                bool turn_Left_Comleate=false;
+                // State 
                 int state=0;
-                int turnState=0;
+                // counter 1 
                 int count1=0;
+                // counter 2
                 int count2=0;
+                // the distance for calculating travel path
+                double distance;
         Driver::Driver(const int32_t &argc, char **argv) :
             ConferenceClientModule(argc, argv, "Driver") {
         }
@@ -86,89 +90,122 @@ namespace msv {
                 //cerr << "Most recent steering data: '" << sd.toString() << "'" << endl;
 
 
-
-                // Design your control algorithm here depending on the input data from above.
-
-
-
                 // Create vehicle control data.
                 VehicleControl vc;
                 double desiredSteeringWheelAngle;
-
-                //start with laneflowing
-                //if(sbd.getValueForKey_MapOfDistances(3)<0){
-                //vc.setSpeed(2);
-                //desiredSteeringWheelAngle=sd.getExampleData();
-
+                distance= vd.getAbsTraveledPath();
+                 
                 // With setSpeed you can set a desired speed for the vehicle in the range of -2.0 (backwards) .. 0 (stop) .. +2.0 (forwards)
-                 if((sbd.getValueForKey_MapOfDistances(3)<0 || sbd.getValueForKey_MapOfDistances(3)>6.8 ) && sbd.getValueForKey_MapOfDistances(5)<0 && obstacleFront==false && startTurningToLeft==false) {
-                        cout<< "Moving" << endl;
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                //  When car is moving in a distance less than 90 or bigger than 150 we are in straight
+                if(distance<90 || distance>150){
+                // lane following when US_FC is not detecting or has distance that is less then 10 and when IR_FR is not detecting
+                if((sbd.getValueForKey_MapOfDistances(3)<0 || sbd.getValueForKey_MapOfDistances(3)>10 ) && sbd.getValueForKey_MapOfDistances(0)<0 && obstacleFront==false && startTurningToLeft==false) {
+                        cout<< "tracking" << endl;
                         vc.setSpeed(2);
                         desiredSteeringWheelAngle=sd.getExampleData();
                        
-                        
-                }
-                else if(sbd.getValueForKey_MapOfDistances(3)>0 && sbd.getValueForKey_MapOfDistances(3)<6.8 && obstacleFront==false && startTurningToLeft==false) {
+                //Obstacle detected from front when 0<US_FC<10. 
+                } else if(sbd.getValueForKey_MapOfDistances(3)>0 && sbd.getValueForKey_MapOfDistances(3)<10 && obstacleFront==false && startTurningToLeft==false) {
                         cout<< "Changing boolean"<<endl;
                         obstacleFront=true;
                         state=1;
 
-                    }else if(state==1 && obstacleFront==true) { // check if obstacle is found in front
+                //changing boolean turning to the left to true in this statement
+                }else if(state==1 && obstacleFront==true) { // check if obstacle is found in front
                         cout<< "obstacle found" << endl;
                         startTurningToLeft=true;
                         obstacleFront=false;
-                      
-                }else if(startTurningToLeft==true && sbd.getValueForKey_MapOfDistances(3)<6.8 && sbd.getValueForKey_MapOfDistances(0)<0){
-                                    cout<< "Start turning 1" << endl;
-                                    desiredSteeringWheelAngle--;
-                                    vc.setSpeed(1);
-                                    state=2;
-                   
-                      
-                }else if(state==2 && sbd.getValueForKey_MapOfDistances(0)>0 && sbd.getValueForKey_MapOfDistances(2)<0){//&& sbd.getValueForKey_MapOfDistances(5)<0){
-                                    vc.setSpeed(1);
-                                   cout<< "Start turning 2" << endl;
-                                   startTurningToLeft=false;
-                                   desiredSteeringWheelAngle--; 
-                                  
+                
+                // in this statement car start to turn to the left with -20 degree and count2 increase until it reaches 100
+                }else if(state==1 && startTurningToLeft==true && count2<100){
+                        cout<< "turning to left" << endl;
+                        desiredSteeringWheelAngle=-20;
+                        vc.setSpeed(1);
+                        count2++;
 
-                }   else if( state==3 && sbd.getValueForKey_MapOfDistances(2)>0 && sbd.getValueForKey_MapOfDistances(5)<0){
-                                    vc.setSpeed(1);
-                                   cout<< "Start turning 3" << endl;
-                                   startTurningToLeft=false;
-                                   desiredSteeringWheelAngle++; 
-                                   //count1++;
-                                   state=4;
+                // end of state1 
+                }else if(state==1){
+                        state=2;
 
-                    
-                }else if(state==4 && sbd.getValueForKey_MapOfDistances(5)>0){
-                                   cout<< "we are in lane following" << endl;
-                                   desiredSteeringWheelAngle=sd.getExampleData(); 
-                                   //count1++;
-                                   vc.setSpeed(2);
-                                   //state=4;
+                //checking whether US_FR IR_FR or IR_RR is detecting obstacle, then switch to follow the left lane
+                }else if(state ==2 && (sbd.getValueForKey_MapOfDistances(4)>0 || sbd.getValueForKey_MapOfDistances(2)>0 || sbd.getValueForKey_MapOfDistances(0)>0 )){
+                        cout<< "lane following in left side" << endl;
+                        vc.setSpeed(2);
+                        desiredSteeringWheelAngle=sd.getExampleData();
+
+                //checking IR_FR and US_FR are not detecting, the car has passed the obstacle and start to steer back with 15 degrees to follow 
+                // the right lane again when the counter reaches 50.
+                }else if(state ==2 && sbd.getValueForKey_MapOfDistances(0)<0 && sbd.getValueForKey_MapOfDistances(4)<0 && count1<50){
+                        cout<< "turning back to right lane" << endl;
+                        vc.setSpeed(2);
+                        desiredSteeringWheelAngle=15;
+                        count1++;
                         
-
-                }else if (sbd.getValueForKey_MapOfDistances(0)>0 && sbd.getValueForKey_MapOfDistances(2)>0 && sbd.getValueForKey_MapOfDistances(5)>0){
-                    cout<< "turning back after overtaking" << endl;
-                    desiredSteeringWheelAngle++;
-                    cout<< desiredSteeringWheelAngle << endl;
-                    vc.setSpeed(1); 
+                // return back to state 0 when the car has completely passed the obstacle.
+                }else{
+                       count1=0;
+                       obstacleFront=false;
+                       startTurningToLeft=false;
+                       state=0;
+                       count2=0;
                 }
-
-                else if (sbd.getValueForKey_MapOfDistances(0)<0 && sbd.getValueForKey_MapOfDistances(2)<0 && sbd.getValueForKey_MapOfDistances(5)>0){
-                    cout<< "turning again to lane " << endl;
-                    desiredSteeringWheelAngle=sd.getExampleData()+3;
-                    cout<< desiredSteeringWheelAngle << endl;
+//__________________________________________________________________________________________________________________________________________________________________________________________________________________
+                // when the car is moving in a distance bigger than 90 and less than 150 we are in curve
+                //(we had to make this if condition as in curve the lane following was not recognizing the vanishing point)
+                // in line 189, we only added a small steering to the left with 15 degrees before adjusting it with lane following after the front detection
+                }else if (distance>90 && distance<150){
+                if((sbd.getValueForKey_MapOfDistances(3)<0 || sbd.getValueForKey_MapOfDistances(3)>10 ) && sbd.getValueForKey_MapOfDistances(0)<0 && obstacleFront==false && startTurningToLeft==false) {
+                    cout<< "tracking" << endl;
                     vc.setSpeed(2);
+                    desiredSteeringWheelAngle=sd.getExampleData();
+                       
+                }else if(sbd.getValueForKey_MapOfDistances(3)>0 && sbd.getValueForKey_MapOfDistances(3)<10 && obstacleFront==false && startTurningToLeft==false) {
+                        cout<< "Changing boolean"<<endl;
+                        obstacleFront=true;
+                        state=1;
+
+                }else if(state==1 && obstacleFront==true) {
+                        cout<< "obstacle found" << endl;
+                        startTurningToLeft=true;
+                        obstacleFront=false;
+                
+                }else if(state==1 && startTurningToLeft==true &&count2<100){
+                        cout<< "turning to left" << endl;
+                        desiredSteeringWheelAngle=-20;
+                        vc.setSpeed(1);
+                        count2++;
+
+                }else if (state==1){
+                        state=2;
+                
+                }else if (state ==2 && (sbd.getValueForKey_MapOfDistances(4)>0 || sbd.getValueForKey_MapOfDistances(2)>0 || sbd.getValueForKey_MapOfDistances(0)>0 )){
+                         cout<< "turn to adjust curve" << endl;
+                        vc.setSpeed(2);
+                        desiredSteeringWheelAngle=15;
+
+                }else if (state ==2 && (sbd.getValueForKey_MapOfDistances(4)>0 || sbd.getValueForKey_MapOfDistances(2)>0 || sbd.getValueForKey_MapOfDistances(0)>0 )){
+                         cout<< "lane following in left side" << endl;
+                        vc.setSpeed(2);
+                        desiredSteeringWheelAngle=sd.getExampleData();
+
+                }else if(state ==2 && sbd.getValueForKey_MapOfDistances(0)<0 && sbd.getValueForKey_MapOfDistances(4)<0 && count1<20){
+                        cout<< "turning back to right lane" << endl;
+                        vc.setSpeed(2);
+                        desiredSteeringWheelAngle=15;
+                        count1++;
+                }else{
+                    count1=0;
+                    obstacleFront=false;
+                    startTurningToLeft=false;
+                    state=0;
+                    count2=0;
                 }
-                 else if (sbd.getValueForKey_MapOfDistances(0)<0 && sbd.getValueForKey_MapOfDistances(2)<0 && sbd.getValueForKey_MapOfDistances(5)<0){
-                    cout<< "lane followig again " << endl;
-                    desiredSteeringWheelAngle= sd.getExampleData();
-                    vc.setSpeed(2);
-                }
-            
+            }
+                //printing out the distance and the states.
                 cout << "                  state :" << state << endl;
+                cout << "                  distance is  :" << distance << endl;
 
                 // With setSteeringWheelAngle, you can steer in the range of -26 (left) .. 0 (straight) .. +25 (right)
                  // 4 degree but SteeringWheelAngle expects the angle in radians!
