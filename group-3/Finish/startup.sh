25c1455343db23e22796a643d19b4a5de8935188
Nicolas Kheirallah <nicolas.kheirallah@gmail.com>
2015-05-24 23:15:44 +0200

    Update and rename group-3/startup.sh to group-3/Finish/startup.sh

--- group-3/Finish/startup.sh
+++ group-3/Finish/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #written by Nicolas Kheirallah
-#Not Tested yet,  will be updated
+#Code too test the software on Odroid or a Lubuntu installation
 /usr/bin/lxterminal -e ./supercomponent --cid=111 
 /usr/bin/lxterminal -e ./cockpit --cid=111
 /usr/bin/lxterminal -e ./camgen --cid=111 --freq=20
