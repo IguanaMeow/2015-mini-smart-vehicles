2289f6b5bf956ec158a8baae8a44a1a7475dcdf4
Luuk van Egeraat <l.vanegeraat@gmail.com>
2015-06-03 14:08:19 +0200

    Final Arduino Sender LLB

--- group-6/Arduino/sending.ino
+++ group-6/Arduino/sending.ino
@@ -1,46 +1,61 @@
+#include <NewPing.h>
+
 // DIGITAL
-#define ECHO_SIDE 8
-#define TRIGGER_SIDE 9
-#define ECHO_FRONT 10
-#define TRIGGER_FRONT 11
+#define ECHO_SIDE 12
+#define TRIGGER_SIDE 13
+#define ECHO_FRONT 9
+#define TRIGGER_FRONT 10
 
 // ANALOG
-#define IR_FRONT 0
-#define IR_SIDE 1
-#define IR_BACK 2
+#define IR_FRONT 1
+#define IR_MIDDLE 2
+#define IR_BACK 3
+
+#define MAX_DISTANCE 2000
+
+
+NewPing sonarFront(TRIGGER_FRONT, ECHO_FRONT, MAX_DISTANCE);
+
+NewPing sonarSide(TRIGGER_SIDE, ECHO_SIDE, MAX_DISTANCE);
+
+int measure(int pin) {
+  return (2914 / (analogRead(pin) + 5)) - 1;
+}
 
 char memory[20];
+int v1, v2, v3, v4, v5, iterator = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(TRIGGER_FRONT, OUTPUT);
   pinMode(TRIGGER_SIDE, OUTPUT);
   pinMode(ECHO_FRONT, INPUT);
   pinMode(ECHO_SIDE, INPUT);
+  delay(100);
 }
 
 void loop() {
+  if(iterator == 0) { v1 = 0; v2 = 0; v3 = 0; v4 = 0; v5 = 0; }
+  
+  v1 += sonarFront.ping_cm();
+  v2 += sonarSide.ping_cm();
+  v3 += measure(IR_FRONT);
+  v4 += measure(IR_MIDDLE);
+  v5 += measure(IR_BACK);
+  
+  iterator++;
+  
+  if(iterator < 10) return;
+ 
+ sprintf(memory, "s%04d%04d%03d%03d%03d", v1 / 10, v2 / 10, v3 / 10, v4 / 10, v5 / 10);
+ 
  int checksum = 0;
- sprintf(memory, "s%04lu%04lu%03d%03d%03d",
-   sonar(ECHO_FRONT, TRIGGER_FRONT),
-   sonar(ECHO_SIDE, TRIGGER_SIDE),
-   analogRead(IR_FRONT),
-   analogRead(IR_SIDE),
-   analogRead(IR_BACK)
- );
  for(int i = 17; i > 0; i--) checksum += (memory[i] - '0');
  memory[18] = String(checksum % 10)[0];
+ 
  Serial.write(memory);
+ 
+ iterator = 0;
+ 
  delay(50);
-}
-
-// measure distance (in cm) based on the speed of sound.
-unsigned long sonar(int echo, int trigger) {
- digitalWrite(trigger, LOW); 
- delayMicroseconds(2); 
- digitalWrite(trigger, HIGH);
- delayMicroseconds(10); 
- unsigned long ms = pulseIn(echo, HIGH) / 2;
- if(ms < 10000) return ms;
- return 9999;
 }
